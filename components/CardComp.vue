51527d2af15f526f168ecf9668f8fc9c0c582b26
ngodingbentar <dewaqintoro@gmail.com>
2023-10-28 01:01:52 +0700

    init

---
+++ components/CardComp.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card bg-gray-100">
+    <div class="content">
+      <div>
+        <!-- <p>hahs</p> -->
+        <p>{{ item.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({ item: { type: Object, required: true } })
+
+</script>
+
+<style lang="postcss" scoped>
+.darkTheme:hover{
+  background: rgb(61, 81, 94) !important;
+}
+.lightTheme:hover{
+  background: #f1f1f1 !important;
+}
+
+.card {
+  @apply px-4 pb-4 rounded-lg my-8;
+  /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.15); */
+  .btn-show {
+    @apply text-center w-full;
+  }
+  .idSurah {
+    @apply items-center justify-center flex rounded-full;
+    width: 45px;
+    height: 45px;
+  }
+  .content {
+    @apply px-4 w-full;
+  }
+}
+
+.arabic {
+  @apply text-right text-3xl;
+  line-height: 4rem !important;
+}
+.my-audio{
+  max-width: 100%;
+  height: 35px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="main text-center">
+    <div>
+      <div v-for="item in thisIndex" :key="item.id">
+        <nuxt-link v-if="item.type === 'int'" :to="item.route">
+          <CardComp :item="item" />
+        </nuxt-link>
+        <a v-else :href="item.route" target="_blank">
+          <CardComp :item="item" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import dataJson from '~/data/myindex.json'
+  import dataExt from '~/data/extIndex.json'
+import CardComp from '../components/CardComp.vue';
+
+  const dataDoa = dataJson.data
+  const extIndex = dataExt.data
+  const thisIndex = computed(() => {
+    const data = [...dataDoa, ...extIndex]
+    const duh = data.sort(function (a, b) {
+      if (a.title < b.title) {
+        return -1
+      }
+      if (a.title > b.title) {
+        return 1
+      }
+      return 0
+    })
+    return duh
+  })
+</script>
+
+
+<style lang="postcss" scoped>
+.darkTheme{
+  /* color: rgb(61, 81, 94); */
+  .card:hover{
+    background: rgb(61, 81, 94);
+  }
+}
+.lightTheme{
+  .card:hover {
+    background: #f1f1f1;
+  }
+}
+.main {
+  @apply pt-24 min-h-screen pb-8 px-40;
+}
+
+.content {
+  @apply my-8;
+}
+.item {
+  @apply px-8 mx-36 my-4;
+}
+.card {
+  @apply text-2xl p-4 rounded-lg;
+}
+</style>
