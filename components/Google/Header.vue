3fd5aa4f1adf3da7a1b7c0892963ca0fc6e7f9af
ngodingbentar <dewaqintoro@gmail.com>
2023-11-05 17:27:00 +0700

    added(init): google search

---
+++ components/Google/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="flex justify-between">
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Search...">
+      <div class="shadow appearance-none border rounded bg-gray-100 items-center flex px-4 ml-4">
+        <button class="">Search</button>
+      </div>
+    </div>
+    <GoogleTab />
+  </div>
+</template>
---
+++ components/Google/Tab.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="mt-4">
+    <div class="flex">
+      <div class="mx-1 cursor-pointer" @click="setTab('all')">
+        <div class="px-4">All</div>
+        <div v-if="activeTab === 'all'" class="h-1 bg-blue-500"></div>
+      </div>
+      <div class="mx-1 cursor-pointer" @click="setTab('images')">
+        <div class="px-4">Images</div>
+        <div v-if="activeTab === 'images'" class="h-1 bg-blue-500"></div>
+      </div>
+      <div>Images</div>
+      <div>Videos</div>
+      <div>News</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const router = useRouter()
+const activeTab = ref('all')
+
+function setTab (tab) {
+  activeTab.value = tab
+  router.replace({
+    query: {
+      tab,
+      ...router.query
+    }
+  })
+}
+</script>
