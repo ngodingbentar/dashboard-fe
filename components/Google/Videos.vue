fec83dbfb9c94451c0770bc324c46c2923425c6a
ngodingbentar <dewaqintoro@gmail.com>
2023-11-06 01:56:13 +0700

    updated: link

--- components/Google/Videos.vue
+++ components/Google/Videos.vue
@@ -8,12 +8,13 @@ import type ImagesVue from './Images.vue';
       About {{ data.length }} results
     </div>
     <div v-for="(item, i) in data" :key="i" class="mb-8">
-      <NuxtLink :to="item.href" target="_blank" class="text-blue-700 font-medium ">
+      <NuxtLink :to="item.content" target="_blank" class="text-blue-700 font-medium ">
         {{ item.title }}
       </NuxtLink>
       <div class="flex mt-4 relative">
         <span class="absolute text-xs text-white bg-gray-600 bg-opacity-60 px-2 rounded-lg bottom-2 left-2 ">{{ item.duration }}</span>
         <img :src="item.images.small" alt="img" class="w-[150px] rounded-lg" />
+        
         <div class="ml-4 ">
           <p class="mb-2 truncate ...">
             {{ item.description }}
